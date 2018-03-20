96d8d26f69cf065cecf1aa262b477cb91d437e22
Naruepanat <cstaytay@hotmail.com>
2018-03-20 15:29:19 +0700

    add api

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,17 @@
-const API_KEY='a53267af2762146666ab36ecb2acd64f';
+import axios from 'axios';
+
+const API_KEY = 'a53267af2762146666ab36ecb2acd64f';
+const ROUTE_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+
+export const FETCT_WEATHER='FETCH_WEATHER';
+
+export function fetchWeather(city){
+
+    const url = `${ROUTE_URL}&q=${city},th`;
+    const request = axios.get(url);
+
+    return{
+        type:FETCT_WEATHER,
+        payload:request
+    };
+}
